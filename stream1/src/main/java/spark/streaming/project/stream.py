c4ce40dbac20f5f2f5e2ab2d24d61d9fb2d3f001
Imenkaabachi <imen.kaabachi@insat.u-carthage.tn>
2024-04-30 11:34:31 +0100

    streaming/spark-kafka

---
+++ stream1/src/main/java/spark/streaming/project/stream.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import csv
+import time
+import random
+
+# Start the nc command
+process = subprocess.Popen(["nc", "-lk", "9999"], stdin=subprocess.PIPE)
+
+csv_file_path = os.path.join(
+      "..", "..", "..", "..", "resources", "input", "customers.csv"
+)
+
+# Open the CSV file
+with open(
+        csv_file_path,
+        "r",
+) as file:
+    reader = csv.reader(file)
+
+    # Convert the reader to a list and shuffle it
+    rows = list(reader)
+    random.shuffle(rows)
+
+    for row in rows:
+        # Convert the row to a string and add a newline character
+        data = ",".join(row) + "\n"
+
+        # Send the data
+        process.stdin.write(data.encode())
+        process.stdin.flush()
+        print(f"Sent: {data.strip()}")
+        time.sleep(1)
+
+# Close the stdin pipe
+process.stdin.close()
+
+# Wait for the nc command to terminate
+process.wait()
